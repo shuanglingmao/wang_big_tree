a0372d12f6b8ba374815158be71aa8aee05796fc
毛双领 <shuangling.mao@ucarinc.com>
2018-06-14 17:53:53 +0800

    新建项目

---
+++ msl/src/main/java/HelloWord.java
@@ -0,0 +1,11 @@
+/**
+ * Description: ${todo}
+ *
+ * @author shuangling.mao
+ * @date 2018/6/14 17:52
+ */
+public class HelloWord {
+    public static void main(String[] args) {
+        System.out.println("HelloWord!");
+    }
+}
