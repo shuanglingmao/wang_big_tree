1e266241a60a90726b1f139a64e286b4e1c7ed13
刘一博 <yibo.liu@ucarinc.com>
2019-06-12 18:11:45 +0800

    完善爬虫

--- CatChat/spring-boot-websocket/src/test/java/com/neo/ReptileUtils.java
+++ CatChat/spring-boot-websocket/src/test/java/com/neo/ReptileUtils.java
@@ -3,8 +3,6 @@ package com.neo;
 import java.io.*;
 import java.net.URL;
 import java.net.URLConnection;
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * description:爬取图片工具
@@ -16,13 +14,13 @@ import java.util.List;
 public class ReptileUtils {
 
     public static void main(String[] args) {
-        reptile("http://www.baidu.com",false,"E:");
+        reptile("http://www.baidu.com","E:\\");
     }
 
 
-    public static void reptile(String strUrl,Boolean isPicture,String file){
+    public static void reptile(String strUrl,String file){
         try {
-            System.out.println("測試"+strUrl);
+            System.out.println("測試爬取"+strUrl);
             URL url=new URL(strUrl);
             //通过url建立与网页的连接
             URLConnection conn=url.openConnection();
@@ -33,35 +31,50 @@ public class ReptileUtils {
             //因此用BufferedReader和InputStreamReader把字节流转化为字符流的缓冲流
             //进行转换时，需要处理编码格式问题
             BufferedReader br=new BufferedReader(new InputStreamReader(is,"UTF-8"));
-            OutputStreamWriter bo = new OutputStreamWriter(new FileOutputStream(file));
             //按行读取并打印
             String line=null;
             while((line=br.readLine())!=null){
-                if (isPicture){
-                    bo.write(line);
-                }else{
-                    if (line.contains(".png") || line.contains(".jpg")){
-                        while(true){
-                            int src = line.indexOf("src");
-                            int jpg = line.indexOf(".jpg");
-                            int png = line.indexOf(".png");
-                            if (jpg>png || jpg == -1){
-                                jpg = png;
-                            }
-                            if (src == 0 || jpg == -1) {
-                                break;
-                            }
-                            reptile("http://"+line.substring(src,jpg+4).split("//")[1],true,file);
-                            line = line.substring(jpg+5,line.length());
+                if (line.contains(".png") || line.contains(".jpg")){
+                    while(true){
+                        int src = line.indexOf("src");
+                        int jpg = line.indexOf(".jpg");
+                        int png = line.indexOf(".png");
+                        if (jpg>png || jpg == -1){
+                            jpg = png;
                         }
+                        if (src == 0 || jpg == -1) {
+                            break;
+                        }
+                        reptileImg("http://"+line.substring(src,jpg+4).split("//")[1],file+ System.currentTimeMillis()+line.substring(jpg,jpg+4));
+                        line = line.substring(jpg+5,line.length());
                     }
                 }
             }
             br.close();
-            bo.close();
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
+
+    public static void reptileImg(String strUrl,String file) throws IOException {
+        System.out.println("測試下载"+strUrl);
+        URL url = new URL(strUrl);
+        //通过url建立与网页的连接
+        URLConnection conn=url.openConnection();
+        //通过链接取得网页返回的数据
+        InputStream is=conn.getInputStream();
+        System.out.println(conn.getContentEncoding());
+        //一般按行读取网页数据，并进行内容分析
+        //因此用BufferedReader和InputStreamReader把字节流转化为字符流的缓冲流
+        //进行转换时，需要处理编码格式问题
+        FileOutputStream fileOutputStream = new FileOutputStream(file);
+        byte[] bs = new byte[2000];
+        while (is.read(bs) != -1){
+            fileOutputStream.write(bs);
+        }
+        is.close();
+        fileOutputStream.close();
+    }
+
 
 }
